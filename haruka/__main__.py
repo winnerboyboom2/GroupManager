6935c345adfed9362cbe35b083377bff30275a3a
winnerboyboom2 <72849880+winnerboyboom2@users.noreply.github.com>
2020-10-17 15:51:33 +0530

    Update __main__.py

--- haruka/__main__.py
+++ haruka/__main__.py
@@ -29,9 +29,7 @@ I'm group manager bot and Anti-spam for RR Players🃏
 Activate Anti-spam protection in your BM by this command : 
 /antispam on 
 
-Made from the source code of [Aditya 🇮🇳](t.me/xditya)
-
-Special thanks to [this person](t.me/denver02) for helping me.
+Made from the source code of [BOOM🇮🇳](t.me/winner_boy_boom)
 
 Deploy a bot like me [now](www.github.com/xditya/GroupManager)
 
@@ -39,7 +37,7 @@ Watch [video tutorial](https://www.youtube.com/watch?v=gXXFpTAk6Vo&feature=youtu
 
 Click /help or Help button below to find out more about how to use me to my full potential.
 
- Add me to a group by clicking [here](http://t.me/tg_GroupManagerBot?startgroup=true).
+ Add me to a group by clicking [here](http://t.me/tg_Boom_group_mangager_bot?startgroup=true).
 """
 
 
